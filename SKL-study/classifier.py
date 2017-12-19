e0c62e360cdab124be7258195e73a18e6edbeacd
Ij888 <ij.ofili@gmail.com>
2017-12-19 13:41:34 +0000

    added entry

--- SKL-study/classifier.py
+++ SKL-study/classifier.py
@@ -1,5 +1,16 @@
-import sklearn
+from sklearn import tree
 
 #training data
-features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"], [120, "smooth"]]
-labels = ["apple", "apple", "orange", "orange" ]
+#features = [[140, "smooth"], [130, "smooth"], [150, "bumpy"], [170, "bumpy"], [120, "smooth"]]
+#labels = ["apple", "apple", "orange", "orange" ]
+
+# as SKL uses real-valued features. As in numbers, change all the strings to integers, int.
+features = [[140, 1], [130, 1], [150, 0], [120, 0]]
+labels = [1, 1, 0, 0, ]
+
+#classifier
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print (clf.predict([[150, 1]]))
+
+
